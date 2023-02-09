175f2bc95756f0f41ffe36416b21a97ce426d5a5
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-02-08 21:46:49 -0500

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Kiho Okazawa on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

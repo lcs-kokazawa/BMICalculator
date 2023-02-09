e8c920edd0b13cf799e74db6afe81627f0b49f4b
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-02-08 22:13:15 -0500

    Deleted a contentView and made a new view called calculationView

---
+++ BMICalculator/Views/CalculationView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculationView.swift
+//  BMICalculator
+//
+//  Created by Kiho Okazawa on 2023-02-08.
+//
+
+import SwiftUI
+
+struct CalculationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculationView()
+    }
+}
